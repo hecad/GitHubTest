f8d23729b9e3e061fa08088688c2e93b4be9aa63
hector <hvaldecantos@gmail.com>
2011-12-21 16:33:49 -0300

    First commit for GitHubTest project

---
+++ src/githubtest/GitHubTest.java
@@ -0,0 +1,15 @@
+package githubtest;
+
+/**
+ *
+ * @author hector
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("GitHub Test!");
+    }
+}
